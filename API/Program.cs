a8b9df9711e5d2815c0ac648adf30bdc3aa28f60
AndrijaTasic <57434030+AndrijaTasic@users.noreply.github.com>
2024-06-15 02:22:01 +0200

    Implemented client side app

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<StoreContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 
 var app = builder.Build();
 
@@ -24,6 +26,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(opt => {
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");         
+});
 
 app.UseAuthorization();
 
