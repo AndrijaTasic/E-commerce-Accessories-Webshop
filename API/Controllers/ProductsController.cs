9198e8e3239d86691a5974001dd473c5d501395a
AndrijaTasic <57434030+AndrijaTasic@users.noreply.github.com>
2024-06-28 00:35:44 +0200

    Added error handling

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Headers;
 using API.Data;
 using API.Entites;
 using Microsoft.AspNetCore.Mvc;
@@ -5,9 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly StoreContext _context;
 
@@ -25,7 +24,12 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+           
+            var product = await _context.Products.FindAsync(id);
+            if(product == null){
+                return NotFound();
+            }
+            return product;
         }
     }
 }
