9a73027957c46a2c5802fe5c11e39341f1c06b75
AndrijaTasic <57434030+AndrijaTasic@users.noreply.github.com>
2024-06-29 03:49:49 +0200

    Added shopping cart features

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -2,14 +2,15 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 
-const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
+const sleep = () => new Promise((resolve) => setTimeout(resolve, 500));
 
 axios.defaults.baseURL = "http://localhost:5000/api/";
+axios.defaults.withCredentials=true;
 
 const responseBody = (response: AxiosResponse) => response.data;
 
 axios.interceptors.response.use(
-  async response => {
+  async (response) => {
     await sleep();
     return response;
   },
@@ -17,10 +18,10 @@ axios.interceptors.response.use(
     const { data, status } = error.response as AxiosResponse;
     switch (status) {
       case 400:
-        if(data.errors){
+        if (data.errors) {
           const modelStateErrors: string[] = [];
-          for(const key in data.errors){
-            if(data.errors[key]){
+          for (const key in data.errors) {
+            if (data.errors[key]) {
               modelStateErrors.push(data.errors[key]);
             }
           }
@@ -35,7 +36,7 @@ axios.interceptors.response.use(
         toast.error(data.title);
         break;
       case 500:
-        router.navigate('/server-error', {state: {error:data}});
+        router.navigate("/server-error", { state: { error: data } });
         break;
       default:
         break;
@@ -63,8 +64,16 @@ const TestErrors = {
   getValidationError: () => requests.get("buggy/validation-error"),
 };
 
+const Basket = {
+  get: () => requests.get("basket"),
+  addItem: (productId: number, quantity = 1) => requests.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
+  removeItem: (productId: number, quantity = 1) =>
+    requests.delete(`basket?productId=${productId}&quantity=${quantity}`)
+};
+
 const agent = {
   Catalog,
   TestErrors,
+  Basket
 };
 export default agent;
